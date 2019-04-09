d772330e22be2778a964334e985ad7d21b528945
gmorenofigols <36441200+gmorenofigols@users.noreply.github.com>
2019-04-09 22:48:14 +0200

    Update forms.py

--- storage_go_project/storage_go_app/forms.py
+++ storage_go_project/storage_go_app/forms.py
@@ -14,3 +14,17 @@ class LoginForm(AuthenticationForm):
     password = forms.CharField(label='', widget=forms.PasswordInput(attrs={'placeholder': u'Contraseña'}))
 
 
+def get_custom_form(entry_model, entry_fields):
+    """
+    """
+
+    fields_list = []
+    [fields_list.append(field.name) for field in entry_fields]
+
+    class _CustomForm(forms.ModelForm):
+
+        class Meta:
+            model = entry_model
+            fields = fields_list
+
+    return _CustomForm
